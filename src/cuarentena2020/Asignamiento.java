ddf716a1fd9870358de115fdb8466155359a928b
owenwilson <owenwilson76467963@gmail.com>
2020-04-17 00:36:29 -0400

    ejemplos de asignamientos y archivo gitignore agregado

---
+++ src/cuarentena2020/Asignamiento.java
@@ -0,0 +1,29 @@
+package cuarentena2020;
+
+public class Asignamiento {
+
+	public static void main(String[] args) {
+		// These are my exampless
+		int num;
+		String name;
+		num = 10;
+		name = "example string";
+		System.out.println("num is assigned "+num);
+		System.out.println("name is assigned "+name);
+		
+		int num1, num2;
+		num1 = 10;
+		num2 = 20;
+		System.out.println("num1= "+num1);
+		System.out.println("num2= "+num2);
+		num1+=num2;
+		System.out.println("num1= " + num1);
+		num1*=num2;
+		System.out.println("resNum1= "+num1);
+		num1/=num2;
+		System.out.println("resNum1= "+num1);
+		num1%=num2;
+		System.out.println("resNum1= "+num1);
+	}
+
+}
